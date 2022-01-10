c516f56785f77d4e9bf039dd76d7747d1c35d843
George Zhai <glzhai2@illinois.edu>
2022-01-10 16:35:25 -0600

    new_products

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,11 @@ import React from "react";
 import "./Home.css";
 import Product from "./Product";
 import biubiu from "./biubiu.jpg";
+import timemachine from "./time_machine.jpg";
+import pork from "./pork.jpg";
+import swim from "./swim.jpg";
+import feast from "./feast.jpg";
+import teleporter from "./teleporter.jpg";
 
 function Home() {
   return (
@@ -21,43 +26,43 @@ function Home() {
             rating={5}
           />
           <Product
-            id={8012021}
-            title="Biubiu: the British long hair"
-            price={19.99}
-            image={biubiu}
+            id={8012020}
+            title="George homemade braised pork in the brown sauce"
+            price={150.59}
+            image={pork}
             rating={5}
           />
           <Product
             id={8012021}
-            title="Biubiu: the British long hair"
-            price={19.99}
-            image={biubiu}
-            rating={5}
-          />
-        </div>
-        <div className="home_row">
-          <Product
-            id={8012021}
-            title="Biubiu: the British long hair"
-            price={19.99}
-            image={biubiu}
-            rating={5}
-          />
-          <Product
-            id={8012021}
-            title="Biubiu: the British long hair"
-            price={19.99}
-            image={biubiu}
+            title="George's exclusive swim lesson"
+            price={2200.0}
+            image={swim}
             rating={5}
           />
         </div>
         <div className="home_row">
           <Product
-            id={8012021}
-            title="Biubiu: the British long hair"
-            price={19.99}
-            image={biubiu}
+            id={30002021}
+            title="Teleporter 3000: Teleport to whereever you want"
+            price={9.99}
+            image={teleporter}
+            rating={1}
+          />
+          <Product
+            id={112020}
+            title="满汉全席"
+            price={1999.99}
+            image={feast}
             rating={5}
+          />
+        </div>
+        <div className="home_row">
+          <Product
+            id={20223022}
+            title="Time machine to 3022"
+            price={199.99}
+            image={timemachine}
+            rating={3}
           />
         </div>
       </div>
